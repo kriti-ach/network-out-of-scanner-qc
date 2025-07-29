95b0d33ce410ca27ada9f8c738354a49fade41a4
kriti-a567 <kriti.a.64@gmail.com>
2025-07-29 08:47:09 -0700

    tried to debug stop+nback

--- src/network-out-of-scanner-qc/utils/utils.py
+++ src/network-out-of-scanner-qc/utils/utils.py
@@ -919,10 +919,7 @@ def compute_stop_signal_metrics(df, dual_task = False, paired_task_col=None, pai
                         n_back_condition = parts[0]
                         delay = parts[1].replace('back', '')
                         paired_mask = (df['n_back_condition'] == n_back_condition) & (df['delay'] == delay)
-                        print(n_back_condition, delay)
-                        print(type(n_back_condition), type(delay))
-                        print(type(df['n_back_condition']), type(df['delay']))
-                        print(paired_mask)
+                        print(any(paired_mask))
                     else:
                         continue
                 else:
