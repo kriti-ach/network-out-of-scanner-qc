6622ed87f53bae57e36f17a646b22ace86b521ed
kriti-a567 <kriti.a.64@gmail.com>
2025-07-29 08:53:28 -0700

    tried to debug stop+nback

--- src/network-out-of-scanner-qc/utils/utils.py
+++ src/network-out-of-scanner-qc/utils/utils.py
@@ -922,6 +922,8 @@ def compute_stop_signal_metrics(df, dual_task = False, paired_task_col=None, pai
                         # check if any paired_mask is true
                         if not paired_mask.any():
                             print(f"No paired mask found for {paired_cond}")
+                            print(df['n_back_condition'].unique())
+                            print(df['delay'].unique())
                     else:
                         continue
                 else:
