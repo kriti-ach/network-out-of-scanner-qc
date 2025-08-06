d22687c060abe1fedeac497576f33f0a04b5c168
kriti-a567 <kriti.a.64@gmail.com>
2025-08-06 12:53:23 -0700

    changed metrics for stop+cued because the data is messed up

--- src/network-out-of-scanner-qc/utils/utils.py
+++ src/network-out-of-scanner-qc/utils/utils.py
@@ -1131,11 +1131,11 @@ def calculate_dual_stop_signal_condition_metrics(df, paired_cond, paired_mask, s
     stop_succ_mask = stop_mask & (df['key_press'] == -1)
 
     # RTs
-    metrics[f'{paired_cond}_go_rt'] = df.loc[go_mask & (df['rt'].notna()), 'rt'].mean()
-    metrics[f'{paired_cond}_stop_fail_rt'] = df.loc[stop_fail_mask & (df['rt'].notna()) & (df['rt'] > 0), 'rt'].mean()
+    metrics[f'{paired_cond}_go_rt'] = df.loc[go_mask & df['rt'].notna(), 'rt'].mean()
+    metrics[f'{paired_cond}_stop_fail_rt'] = df.loc[stop_fail_mask & df['rt'].notna() & (df['rt'] > 0), 'rt'].mean()
 
     # Accuracies
-    metrics[f'{paired_cond}_go_acc'] = df.loc[go_mask, df['key_press'] == df['correct_response']].mean()
+    metrics[f'{paired_cond}_go_acc'] = df.loc[go_mask & (df['key_press'] == df['correct_response'])].mean()
     go_trials = df[go_mask]
     # Go omission rate
     mask_omission = go_mask & (df['key_press'] == -1)
